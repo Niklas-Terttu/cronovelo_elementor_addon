75e5593b10de8a7fabe5bd637b6679a592e8cd17
Niklas-Terttu <niklas@terttus.dk>
2026-06-14 20:53:30 +0200

    Bump version to 1.0.6

--- cronovelo-addons.php
+++ cronovelo-addons.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Cronovelo Addons
  * Description: Skræddersyede avancerede Elementor widgets fra Cronovelo.
- * Version:     1.0.5
+ * Version:     1.0.6
  * Author:      Cronovelo
  * Text Domain: cronovelo-addons
  */
